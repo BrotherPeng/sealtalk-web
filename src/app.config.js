68707ce902d445424957d75fee574f1cd5259187
yangchuan <qwerchuan@foxmail.com>
2016-09-27 16:11:21 +0800

    修改 样式

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,11 @@
+// window.__sealtalk_config={
+//   serverUrl:"http://api.sealtalk.im",
+//   appkey:"n19jmcy59f1q9"
+// }
 window.__sealtalk_config={
-  serverUrl:"http://api.sealtalk.im",
-  appkey:"n19jmcy59f1q9"
+  // serverUrl:"***",
+  // appkey:"***"
+  serverUrl:"http://api.hitalk.im",
+  appkey:"e0x9wycfx7flq",
+  // appkey:"n19jmcy59f1q9"
 }
